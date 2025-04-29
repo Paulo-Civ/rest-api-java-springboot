667dff0e86ab1fc66ea49dbc3bb37017187a8913
Paulo Civardi <paulocivardi@gmail.com>
2025-04-29 17:22:49 -0400

    Verbs HTTP - GET/POST/PUT/DELETE

--- rest-api-java-springboot/src/main/java/com/pauloCiv/PersonServices.java
+++ rest-api-java-springboot/src/main/java/com/pauloCiv/PersonServices.java
@@ -4,6 +4,8 @@ import com.pauloCiv.model.Person;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,19 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        logger.info("Finding all People");
+
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i=0; i<8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
 
     public Person findById(String id){
         logger.info("Finding one Person!");
@@ -23,6 +38,36 @@ public class PersonServices {
         person.setLastName("Civardi");
         person.setAddress("Dourados - Mato Grosso do Sul - Brasil");
         person.setGender("male");
+
+        return person;
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting one Person!");
+
+    }
+
+    private Person mockPerson(int i) {
+
+        //Mock
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName "+ i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male");
 
         return person;
     }
