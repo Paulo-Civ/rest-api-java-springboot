e4f64a5c45e5f6f12ab740c8243d2a64c5ba3097
Paulo Civardi <paulocivardi@gmail.com>
2025-05-07 20:15:51 -0400

    Migrations with Flyway

--- rest-api-java-springboot/src/main/java/com/pauloCiv/controllers/TestLogController.java
+++ rest-api-java-springboot/src/main/java/com/pauloCiv/controllers/TestLogController.java
@@ -4,10 +4,12 @@ import com.pauloCiv.services.PersonServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "/api/test/v1")
 public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
---
+++ rest-api-java-springboot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Porbandar - Índia', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - Índia', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(12, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
