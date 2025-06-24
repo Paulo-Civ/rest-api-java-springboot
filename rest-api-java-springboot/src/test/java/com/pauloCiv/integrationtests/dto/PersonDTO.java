466ff7178c9de3a8b4e5829e0e5a9151eb1811e4
Paulo Civardi <paulocivardi@gmail.com>
2025-06-24 19:06:46 -0400

    Desafio_S19_142 - Automated tests with XML

--- rest-api-java-springboot/src/test/java/com/pauloCiv/integrationtests/dto/PersonDTO.java
+++ rest-api-java-springboot/src/test/java/com/pauloCiv/integrationtests/dto/PersonDTO.java
@@ -1,8 +1,14 @@
 package com.pauloCiv.integrationtests.dto;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PersonDTO implements Serializable{
     private static final long serialVersionUID = 1L;
 
